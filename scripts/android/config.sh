c8594ac884fa346b89d743bd1dd6160925fdce0a
Josh Babb <sneurlax@gmail.com>
2023-05-11 20:46:06 -0500

    use ndk pkg-config bins and specify ranlib (for linux) TODO specify ranlib for android etc

--- scripts/android/config.sh
+++ scripts/android/config.sh
@@ -11,8 +11,8 @@ export API=21
 #r21e also works and was the preferred before
 
 # Only choose one of these, depending on your build machine...
-#export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/darwin-x86_64
-export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
+#export TOOLCHAIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64
+export TOOLCHAIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64
 
 # Only choose one of these, depending on your device...
 #export TARGET=aarch64-linux-android
@@ -20,10 +20,14 @@ export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
 #export TARGET=i686-linux-android
 export TARGET=x86_64-linux-android
 
+ARCH=linux-x86_64
 export AR=$TOOLCHAIN/bin/llvm-ar
 export CC=$TOOLCHAIN/bin/$TARGET$API-clang
 export AS=$CC
 export CXX=$TOOLCHAIN/bin/$TARGET$API-clang++
 export LD=$TOOLCHAIN/bin/ld
-export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
+export RANLIB=$TOOLCHAIN/bin/x86_64-linux-android-ranlib # TODO change based on arch
 export STRIP=$TOOLCHAIN/bin/llvm-strip
+
+export PKG_CONFIG_PATH=$ANDROID_NDK_HOME/prebuilt/$ARCH/lib/pkgconfig
+export PKG_CONFIG_SYSROOT_DIR=$ANDROID_NDK_HOME/prebuilt/$ARCH/lib/pkgconfig
